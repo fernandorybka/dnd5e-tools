e5e10c6ced09905b31191068fed3b39e1081aa52
Fernando Rybka <fernando.pr@gmail.com>
2021-08-27 01:01:52 -0300

    add: singleton modal and event (signal) emitter

---
+++ src/components/SingleModal.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Emitter from '../services/Emitter';
+import { CardClasses } from '../assets/themes/Colors';
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => (Object.assign(CardClasses,
+  {
+  modal: {
+    transitions: 'ease all 0.6s'
+  },
+})));
+
+
+
+function SingleModal() {
+  const classes = useStyles();
+
+  const [modalContent, setModalContent] = React.useState('');
+  const [modalCustomClass, setModalCustomClass] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  Emitter.on('OPEN_MODAL_SIGNAL', (modalInfo) => handleModalOpenSignal(modalInfo));
+  
+  const handleModalOpenSignal = (modalInfo) => {
+    if (modalInfo.customClass) setModalCustomClass(classes[modalInfo.customClass]);
+
+    setModalContent(modalInfo.content);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll='paper'
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        fullWidth={true}
+        maxWidth={'md'}
+      >
+        <DialogContent dividers={true} className={`${modalCustomClass} ${classes.modal}`}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {modalContent}
+          </DialogContentText>
+        </DialogContent>
+        {/* <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            OK
+          </Button>
+        </DialogActions> */}
+      </Dialog>
+    </div>
+  );
+}
+
+
+export default SingleModal;
