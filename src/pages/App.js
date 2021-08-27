57b02762a5ab7563af3d92fcf807886fb3118655
Fernando Rybka <fernando.pr@gmail.com>
2021-08-27 01:05:43 -0300

    feat: spells page first commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React, { Profiler, useEffect, useState } from "react";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import CardList from "../components/CardList";
+import FeaturedCard from "../components/FeaturedCard";
+import theme from "../assets/themes/Theme";
+import dnd5etoolsdb from "../services/dnd5etoolsdb";
 
-import './App.css';
+import "../assets/css/DnD5eTools.css";
+import SingleModal from "../components/SingleModal";
 
 function App() {
-  return (
-   <box>
+  function onRenderCallback(
+    id, // the "id" prop of the Profiler tree that has just committed
+    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render the entire subtree without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions // the Set of interactions belonging to this update
+  ) {
+    console.log(`duracao: ${actualDuration}`);
+  }
 
-   </box>
+  const [spellsByClass, setSpellsByClass] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let spellsRaw = await dnd5etoolsdb.getAllSpells();
+      let filteredSpellsArray = dnd5etoolsdb.getSpellsByClass(
+        spellsRaw,
+        "Druid"
+      );
+      setSpellsByClass(filteredSpellsArray);
+    };
+
+    loadAll();
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SingleModal />
+      {/* <FeaturedCard /> */}
+      <div style={{marginTop: '0px'}}>
+        <Profiler id="listas" onRender={onRenderCallback}>
+          {spellsByClass.map((spellsByLevel, key) => (
+            <CardList
+              key={key}
+              listName={`Level ${key}`}
+              items={spellsByLevel}
+            />
+          ))}
+        </Profiler>
+      </div>
+    </ThemeProvider>
   );
 }
 
