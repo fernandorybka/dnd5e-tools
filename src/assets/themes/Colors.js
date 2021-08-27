94ed2928dee4865cc4f9a65f5de0dfc5dd9084f0
Fernando Rybka <fernando.pr@gmail.com>
2021-08-27 00:57:22 -0300

    add: themes and colors

---
+++ src/assets/themes/Colors.js
@@ -0,0 +1,45 @@
+export const Colors = {
+    pastelWhite: '#e8e3b0',
+    pastelGray: '#efecca',
+    pastelBlack: '#5e565a', //403B3D
+    pastelBlue: '#a9cbb7',
+    pastelYellow:  '#ffd04e',//'#f7ff58',
+    pastelOrange: '#ff934f',
+    pastelRed: '#ff4f4f',
+    pastelPurple: '#d9bde3',
+    pastelGreen: '#f9fbfa',
+    pastelPink: '#ebbab5',
+    pastelBrown: '#bb947e',
+    pastelLime: '#caf6b2'
+};
+
+export const CardClasses = {
+    cardC: {
+        background: Colors.pastelBlue,
+      },
+      cardA: {
+        background: Colors.pastelOrange,
+      },
+      cardN: {
+        background: Colors.pastelYellow,
+      },
+      cardD: {
+        background: Colors.pastelPurple,
+      },
+      cardV: {
+        background: Colors.pastelPink,
+      },
+      cardI: {
+        background: Colors.pastelLime,
+      },
+      cardT: {
+        background: Colors.pastelRed,
+      },
+      smallDivider: {
+        marginLeft: "10px",
+        marginRight: "70%",
+        marginTop: "-5px",
+        marginBottom: "15px",
+        border: "white solid 2px",
+      },
+};
---
+++ src/assets/themes/Theme.js
@@ -0,0 +1,32 @@
+import { createTheme } from '@material-ui/core/styles';
+import {Colors} from './Colors'
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: Colors.pastelBlue,
+    },
+    secondary: {
+      main: Colors.pastelOrange,
+    },
+    background: {
+      default: Colors.pastelGray,
+      paper: Colors.pastelYellow,
+    },
+    text: {
+      primary: '#DDDDDD',
+    },
+  },
+  typography: {
+    h5: {
+      fontFamily: 'Teko',
+      fontSize: '1.8rem',
+      color: 'white',
+      textTransform: 'uppercase',
+    }
+  }
+});
+
+export default theme;
+//#ffd84b
