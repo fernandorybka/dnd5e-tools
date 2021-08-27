ddc9efe8ec65f401192fc9c6558c0bbfb971b481
Fernando Rybka <fernando.pr@gmail.com>
2021-08-27 01:03:48 -0300

    add: util and SchoolsOfMagic array

---
+++ src/util/SchoolsOfMagic.js
@@ -0,0 +1,10 @@
+export const SchoolsOfMagic = {
+    'C': "Conjuration",
+    'N': "Necromancy",
+    'V': "Evocation",
+    'A': "Abjuration",
+    'T': "Transmutation",
+    'D': "Divination",
+    'E': "Enchantment",
+    'I': "Illusion",
+};
