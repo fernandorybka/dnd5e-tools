0fea22d2b1a397285332cd42a002e7b0e03ba7d4
Fernando Rybka <fernando.pr@gmail.com>
2021-08-31 15:35:13 -0300

    fix: added unsanitized tags

--- src/components/main/SanitizedText.js
+++ src/components/main/SanitizedText.js
@@ -3,7 +3,7 @@ import React from 'react'
 const simpleTagReplace = (stringText, ...tags) => {
   let finalString = stringText;
   tags.map( (tag, index)  => {
-    let reg = new RegExp("{@" + tag + "\\s(\\w+)\\s*}", "g");
+    let reg = new RegExp("{@" + tag + "\\s*(\\w+\\s*)\\s*}", "g");
     finalString = finalString.replace( reg, function(match, tagval) { return `${tagval}`});
   });
   return finalString;
@@ -15,7 +15,7 @@ function SanitizedText({text}) {
     let i;
   
   stringText = simpleTagReplace(stringText, 'skill', 'damage', 'dice', 'creature', 
-  'condition', 'spell', 'sense', 'scaledamage', 'hit', 'chance');
+  'condition', 'spell', 'sense', 'scaledamage', 'hit', 'chance', 'race');
 
     while( (i = stringText.indexOf("{@")) > -1) {
         let specialTag = stringText.substring(i, stringText.indexOf('}', i));
@@ -24,8 +24,7 @@ function SanitizedText({text}) {
                          stringText.substring(i+9,stringText.indexOf('|')) +
                          stringText.substring(stringText.indexOf("}")+1);
         } else {
-            console.log( 'Tag '+specialTag+' unsanitized' );
-            stringText = stringText.replaceAll(specialTag, '')
+            stringText = stringText.replaceAll(specialTag, specialTag.replace('{@', ''))
         }         
     }
     stringText = stringText.replaceAll('}', '');
