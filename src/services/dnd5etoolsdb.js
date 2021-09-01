3159302bde8b56535e9da2486e96ead67840656e
Fernando Rybka <fernando.pr@gmail.com>
2021-09-01 14:59:08 -0300

    add: source filter added

--- src/services/dnd5etoolsdb.js
+++ src/services/dnd5etoolsdb.js
@@ -25,15 +25,6 @@ export default {
             'IDRotF': require('../assets/jsons/spells/spells-idrotf.json'),
             'LLK': require('../assets/jsons/spells/spells-llk.json'),
             'TCE': require('../assets/jsons/spells/spells-tce.json'),
-            // 'UA2020PsionicOptionsRevisited': require('../assets/jsons/spells/spells-ua-2020por.json'),
-            // 'UA2020SpellsAndMagicTattoos': require('../assets/jsons/spells/spells-ua-2020smt.json'),
-            // 'UA2021DraconicOptions': require('../assets/jsons/spells/spells-ua-2021do.json'),
-            // 'UAArtificerRevisited': require('../assets/jsons/spells/spells-ua-ar.json'),
-            // 'UAFighterRogueWizard': require('../assets/jsons/spells/spells-ua-frw.json'),
-            //'UAModernMagic': require('../assets/jsons/spells/spells-ua-mm.json'),
-            // 'UASorcererAndWarlock': require('../assets/jsons/spells/spells-ua-saw.json'),
-            // 'UAStarterSpells': require('../assets/jsons/spells/spells-ua-ss.json'),
-            // 'UAThatOldBlackMagic': require('../assets/jsons/spells/spells-ua-tobm.json'),
             'XGE': require('../assets/jsons/spells/spells-xge.json'),
             'PHB': require('../assets/jsons/spells/spells-phb.json'),
         };
@@ -45,13 +36,20 @@ export default {
         return spellsJson;
     },
     getSpellsByClass: (spellsRaw, filters) => {
-        let myClassSpells = spellsRaw.spell.filter(spell => spell.classes && spell.classes.fromClassList && spell.classes.fromClassList.some(item => item.name === filters.dndClass));
+        // filter by DnD Class
+        let mySpells = spellsRaw.spell.filter(spell => spell.classes && spell.classes.fromClassList && spell.classes.fromClassList.some(item => item.name === filters.dndClass));
+
+        // filter by Source
+        mySpells = mySpells.filter(spell => filters.spellsSources.includes(spell.source));
         
         let spellsByClass = [];
         
+        // filter by Spell Level
         for (let i = filters.spellLevels[0]; i <= filters.spellLevels[1]; i++) {
-            spellsByClass[i] = myClassSpells.filter(spell => spell.level === i);
+            spellsByClass[i] = mySpells.filter(spell => spell.level === i);
         }
+
+
         return spellsByClass;
     }
 }
